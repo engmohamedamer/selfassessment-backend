6ce69a1a46245373d28e17f2fab55160047f72ef
makl <makl@sahl.tech>
2019-10-16 16:43:34 +0200

    fix update user

--- backend/views/user/_form.php
+++ backend/views/user/_form.php
@@ -35,82 +35,51 @@ $model->roles =Yii::$app->session->get('UserRole');
     <div class="col-lg-12">
         <div class="card">
             <div class="card-body">
-
-                <ul class="nav nav-pills">
-                    <li class="nav-item pull-left header">
-                        <h3 class="card-title"><?php echo Yii::t('backend', 'User Details') ?>  </a></h3>
-                    </li>
-                </ul>
-
-
-
                 <div class="row">
                     <div class="col-md-4">
-                        <div class="well">
-                                <?php echo $form->field($profile, 'picture')->widget(\common\b4widget\upload\MyUpload::class, [
-                                    'url'=>['avatar-upload']
-                                ]) ?>
-                        </div>
+                        <?php echo $form->field($profile, 'picture')->widget(\common\b4widget\upload\MyUpload::class, [
+                            'url'=>['avatar-upload']
+                        ]) ?>
                     </div>
                     <div class="col-md-8">
                         <div class="row">
                             <div class="col-md-6">
-                                <div class="well">
-                                        <?php echo $form->field($model, 'email') ?>
-                                </div>
+                                <?php echo $form->field($model, 'email') ?>
                             </div>
                             <div class="col-md-6">
-                                <div class="well">
-                                        <?php echo $form->field($model, 'password')->passwordInput() ?>
-
-                                </div>
-                            </div>
-                        </div>
-                        <div class="row">
-                            <div class="col-md-6">
-                                <div class="well">
-                                        <?php echo $form->field($profile, 'firstname') ?>
-                                </div>
+                                <?php echo $form->field($model, 'password')->passwordInput() ?>
                             </div>
                             <div class="col-md-6">
-                                <div class="well">
-                                        <?php echo $form->field($profile, 'lastname') ?>
-                                </div>
+                                <?php echo $form->field($profile, 'firstname') ?>
+                            </div>
+                            <div class="col-md-6">
+                                <?php echo $form->field($profile, 'lastname') ?>
                             </div>
                         </div>
                     </div>
                 </div>
 
                 <div class="row">
-
-
                     <div class="col-md-4 col-sm-12">
-
                         <?php echo $form->field($profile, 'gender')->dropDownlist([
                             UserProfile::GENDER_FEMALE => Yii::t('backend', 'Female'),
                             UserProfile::GENDER_MALE => Yii::t('backend', 'Male')
                         ]) ?>
                     </div>
-                    
                     <div class="col-md-4 col-sm-12">
                         <?php echo $form->field($profile, 'mobile') ?>
                     </div>
-
-
-
-
                     <div class="col-md-4 col-sm-12">
                         <?php echo $form->field($model, 'status')->dropDownList(User::statuses()) ?>
                     </div>
-
                 </div>
 
+                <div class="row">
+                    <div class="col-md-12">
+                        <?php echo Html::submitButton(Yii::t('backend', 'Save'), ['class' => 'btn btn-primary', 'name' => 'signup-button']) ?>
+                    </div>
+                </div>
             </div>
-        <div class="row">
-            <div class="form-group">
-                <?php echo Html::submitButton(Yii::t('backend', 'Save'), ['class' => 'btn btn-primary', 'name' => 'signup-button']) ?>
-            </div>
-        </div>
 
         </div>
     </div>
