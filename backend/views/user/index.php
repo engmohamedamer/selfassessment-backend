af13b429a276c3cb011617f2714f07c523e27dc9
mohamed.amer <mohamed.amer2050@gmail.com>
2019-10-16 13:39:59 +0200

    fix manager module

--- backend/views/user/index.php
+++ backend/views/user/index.php
@@ -76,34 +76,7 @@ $this->params['breadcrumbs'][] = $this->title;
                     'dataProvider' => $dataProvider,
                     'filterModel' => $searchModel,
                     'columns' => $gridColumns, // check the configuration for grid columns by clicking button above
-                    'containerOptions' => ['style' => 'overflow: auto'], // only set when $responsive = false
-                    'headerRowOptions' => ['class' => 'kartik-sheet-style'],
-                    'filterRowOptions' => ['class' => 'kartik-sheet-style'],
-                    'pjax' => true, // pjax is set to always true for this demo
-                    // set your toolbar
-                    'toolbar' =>  [
-                        [
-                            'content' =>
-                                Html::button('<i class="fas fa-plus"></i>', [
-                                    'class' => 'btn btn-success',
-                                    'title' => Yii::t('kvgrid', 'Add Book'),
-                                    'onclick' => 'alert("This will launch the book creation form.\n\nDisabled for this demo!");'
-                                ]) . ' '.
-                                Html::a('<i class="fas fa-redo"></i>', ['grid-demo'], [
-                                    'class' => 'btn btn-outline-secondary',
-                                    'title'=>Yii::t('kvgrid', 'Reset Grid'),
-                                    'data-pjax' => 0,
-                                ]),
-                            'options' => ['class' => 'btn-group mr-2']
-                        ],
-                        '{export}',
-                        '{toggleData}',
-                    ],  
-                    'toggleDataContainer' => ['class' => 'btn-group mr-2'],
-                    // set export properties
-                    'export' => [
-                        'fontAwesome' => true
-                    ],
+
                 ]);
             ?>
             </div>
