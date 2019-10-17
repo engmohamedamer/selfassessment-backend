304e5c150a7a84d6031278551e78974718d6943a
makl <makl@sahl.tech>
2019-10-17 09:21:16 +0200

    fix

--- backend/views/user/index.php
+++ backend/views/user/index.php
@@ -37,16 +37,7 @@ $this->params['breadcrumbs'][] = $this->title;
 <div class="row">
     <div class="col-lg-12">
         <div class="card">
-            <div class="card-header border-0">
-                <div class="card-tools">
-                    <a href="#" class="btn btn-tool btn-sm">
-                        <i class="fas fa-search"></i>
-                    </a>
-                    <a href="#" class="btn btn-tool btn-sm">
-                        <i class="fas fa-bars"></i>
-                    </a>
-                </div>
-            </div>
+           
             <div class="card-body">
             <?php
                 $gridColumns=[
