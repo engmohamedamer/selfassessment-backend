20025e93d602e06b255d842ae844fa07804e23b4
MohamedAaa <m.3laa.95@gmail.com>
2019-10-17 09:49:42 +0200

    fix error

--- backend/views/organization/_form.php
+++ backend/views/organization/_form.php
@@ -29,7 +29,7 @@ if (isset($model->city_id)) {
 
     <?php $form = ActiveForm::begin(); ?>
 
-    <div class="alert alert-danger">
+    <div class="alert alert-danger error-summary" style="display: none;">
         <?php  echo $form->errorSummary($model); ?>
     </div>
 
