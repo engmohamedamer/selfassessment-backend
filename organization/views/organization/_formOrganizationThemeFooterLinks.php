c8abf2ef966b2c51914d48a198a79fd455d26c88
Mahmoud Baghdady <mbaghdady14@gmail.com>
2019-10-22 15:25:58 +0200

    new fixes

--- organization/views/organization/_formOrganizationThemeFooterLinks.php
+++ organization/views/organization/_formOrganizationThemeFooterLinks.php
@@ -11,50 +11,70 @@ use yii\widgets\ActiveForm;
 
 ?>
 
-<div class="row">
 
-    <div class="col-md-6 col-sm-12">
-        <?= $form->field($organizationFooterLinks, 'name_link1')->textInput(['maxlength' => true, 'placeholder' => 'Name'])->widget(MyMultiLanguageActiveField::className());  ?>
+    <div class='row mb-4' style='    background: #f4f5f9; margin-left: 10px !important; margin-right: 10px !important; padding: 10px 0'>
+        <div class="col-md-6 col-sm-12">
+            <?= $form->field($organizationFooterLinks, 'name_link1')->textInput(['maxlength' => true, 'placeholder' => 'Name'])->widget(MyMultiLanguageActiveField::className());  ?>
+        </div>
+
+        <div class="col-md-6 col-sm-12">
+            <?php echo  $form->field($organizationFooterLinks, 'link1')->textInput() ?>
+        </div>
     </div>
 
-    <div class="col-md-6 col-sm-12">
-        <?php echo  $form->field($organizationFooterLinks, 'link1')->textInput() ?>
+    <div class='row mb-4' style='    background: #f4f5f9; margin-left: 10px !important; margin-right: 10px !important; padding: 10px 0'>
+        <div class="col-md-6 col-sm-12">
+            <?= $form->field($organizationFooterLinks, 'name_link2')->textInput()
+            ->widget(MyMultiLanguageActiveField::className());  ?>
+        </div>
+
+        <div class="col-md-6 col-sm-12">
+            <?php echo  $form->field($organizationFooterLinks, 'link2')->textInput() ?>
+        </div>
     </div>
 
-    <div class="col-md-6 col-sm-12">
-        <?= $form->field($organizationFooterLinks, 'name_link2')->textInput()
-        ->widget(MyMultiLanguageActiveField::className());  ?>
+    <div class='row mb-4' style='    background: #f4f5f9; margin-left: 10px !important; margin-right: 10px !important; padding: 10px 0'>
+        <div class="col-md-6 col-sm-12">
+            <?= $form->field($organizationFooterLinks, 'name_link3')->textInput()
+            ->widget(MyMultiLanguageActiveField::className());  ?>
+        </div>
+
+        <div class="col-md-6 col-sm-12">
+            <?php echo  $form->field($organizationFooterLinks, 'link3')->textInput() ?>
+        </div>
+
     </div>
 
-    <div class="col-md-6 col-sm-12">
-        <?php echo  $form->field($organizationFooterLinks, 'link2')->textInput() ?>
+
+    <div class='row mb-4' style='    background: #f4f5f9; margin-left: 10px !important; margin-right: 10px !important; padding: 10px 0'>
+        <div class="col-md-6 col-sm-12">
+            <?= $form->field($organizationFooterLinks, 'name_link4')->textInput()
+            ->widget(MyMultiLanguageActiveField::className());  ?>
+        </div>
+
+        <div class="col-md-6 col-sm-12">
+            <?php echo  $form->field($organizationFooterLinks, 'link4')->textInput() ?>
+        </div>
+
     </div>
 
-    <div class="col-md-6 col-sm-12">
-        <?= $form->field($organizationFooterLinks, 'name_link3')->textInput()
-        ->widget(MyMultiLanguageActiveField::className());  ?>
+    <div class='row mb-4' style='    background: #f4f5f9; margin-left: 10px !important; margin-right: 10px !important; padding: 10px 0'>
+        <div class="col-md-6 col-sm-12">
+            <?= $form->field($organizationFooterLinks, 'name_link5')->textInput()
+            ->widget(MyMultiLanguageActiveField::className());  ?>
+        </div>
+
+        <div class="col-md-6 col-sm-12">
+            <?php echo  $form->field($organizationFooterLinks, 'link5')->textInput() ?>
+        </div>
+
     </div>
 
-    <div class="col-md-6 col-sm-12">
-        <?php echo  $form->field($organizationFooterLinks, 'link3')->textInput() ?>
-    </div>
+    
 
-    <div class="col-md-6 col-sm-12">
-        <?= $form->field($organizationFooterLinks, 'name_link4')->textInput()
-        ->widget(MyMultiLanguageActiveField::className());  ?>
-    </div>
+    
 
-    <div class="col-md-6 col-sm-12">
-        <?php echo  $form->field($organizationFooterLinks, 'link4')->textInput() ?>
-    </div>
+    
 
-    <div class="col-md-6 col-sm-12">
-        <?= $form->field($organizationFooterLinks, 'name_link5')->textInput()
-        ->widget(MyMultiLanguageActiveField::className());  ?>
-    </div>
+    
 
-    <div class="col-md-6 col-sm-12">
-        <?php echo  $form->field($organizationFooterLinks, 'link5')->textInput() ?>
-    </div>
-
-</div>
