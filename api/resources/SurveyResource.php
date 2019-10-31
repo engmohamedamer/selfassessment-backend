e670d0a8017fbe69606834d1ecddc42301a73103
Mohamed Amer <Mohamed.amer2050@gmail.com>
2019-10-31 12:15:02 +0200

    fix

--- api/resources/SurveyResource.php
+++ api/resources/SurveyResource.php
@@ -108,16 +108,81 @@ class SurveyResource extends Survey
                 }
                 */
                 $userId = \Yii::$app->user->identity->id;
-                $userAnswers =  SurveyUserAnswer::find()->select('*,survey_question.survey_question_type')->joinWith(['question'], true, 'INNER JOIN')->where(['survey_user_answer_user_id'=>$userId,'survey_user_answer_survey_id'=>$model->survey_id])->all();
+//                $userAnswers =  SurveyUserAnswer::find()->select('*,survey_question.survey_question_type')->joinWith(['question'], true, 'INNER JOIN')->where(['survey_user_answer_user_id'=>$userId,'survey_user_answer_survey_id'=>$model->survey_id])->all();
+//                $data = [];
+//                foreach ($userAnswers as $key => $value) {
+//
+//
+//                    if ($value->question->survey_question_type == SurveyType::TYPE_SINGLE_TEXTBOX) {
+//                        if($value->survey_user_answer_value){
+//                            $data[$key] = [$value->survey_user_answer_question_id =>$value->survey_user_answer_value];
+//
+//                        }
+//                    }else{
+//                        if($value->survey_user_answer_value) {
+//                            $data[$key] = [$value->survey_user_answer_question_id => $value->survey_user_answer_answer_id];
+//                        }
+//
+//                    }
+//                }
+
                 $data = [];
-                foreach ($userAnswers as $key => $value) {
-                    if ($value->question->survey_question_type == SurveyType::TYPE_SINGLE_TEXTBOX) {
-                        $data[] = [$value->survey_user_answer_question_id =>$value->survey_user_answer_value];
-                    }else{
-                        $data[] = [$value->survey_user_answer_question_id =>$value->survey_user_answer_answer_id];
+                //get survey questions then check user answers
+
+                foreach ($model->questions as  $question) {
+                    //echo $question->survey_question_id.'<br>';
+                    // has one value
+                    if ($question->survey_question_type === SurveyType::TYPE_ONE_OF_LIST
+                        || $question->survey_question_type === SurveyType::TYPE_DROPDOWN
+                        || $question->survey_question_type === SurveyType::TYPE_SLIDER
+                        || $question->survey_question_type === SurveyType::TYPE_SINGLE_TEXTBOX
+                        || $question->survey_question_type === SurveyType::TYPE_COMMENT_BOX
+                    ){
+                        //fetch user answers
+                        $userAnswerObj = SurveyUserAnswer::findOne([
+                            'survey_user_answer_user_id'=>$userId,
+                            'survey_user_answer_survey_id'=>$model->survey_id,
+                            'survey_user_answer_question_id'=>$question->survey_question_id
+
+                        ]);
+                        if($userAnswerObj){
+                            $data[$question->survey_question_id] = $userAnswerObj->survey_user_answer_value;
+
+                        }
+
+                    }else if(
+                        $question->survey_question_type === SurveyType::TYPE_MULTIPLE
+                        || $question->survey_question_type === SurveyType::TYPE_RANKING
+                        || $question->survey_question_type === SurveyType::TYPE_MULTIPLE_TEXTBOX
+                        || $question->survey_question_type === SurveyType::TYPE_DATE_TIME
+                        || $question->survey_question_type === SurveyType::TYPE_CALENDAR
+                    ){
+
+                        //fetch user answers
+                        $userAnswersObj = SurveyUserAnswer::find()->where([
+                            'survey_user_answer_user_id'=>$userId,
+                            'survey_user_answer_survey_id'=>$model->survey_id,
+                            'survey_user_answer_question_id'=>$question->survey_question_id
+
+                        ])->all();
+                        if($userAnswersObj){
+                            foreach ($userAnswersObj as $item) {
+                                if($item->survey_user_answer_answer_id){
+                                    $data[$question->survey_question_id][] = $item->survey_user_answer_answer_id;
+                                }
+
+                            }
+
+
+
+                        }
+
 
                     }
+
                 }
+
+
                 return $data;
             }
         ];
