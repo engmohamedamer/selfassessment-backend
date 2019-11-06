06993de597f6474122140e00d13064ed5ddc23f0
MohamedAaa <m.3laa.95@gmail.com>
2019-11-06 17:15:48 +0200

    answer list type ranking

--- api/resources/SurveyResource.php
+++ api/resources/SurveyResource.php
@@ -143,7 +143,7 @@ class SurveyResource extends Survey
                             $columns = [];
                             foreach ($question->answers as $index => $value) {
                                 $qAnswer[] = ['value'=>$value->survey_answer_id,'text'=> $value->survey_answer_name];
-                                $text =index+1 ;
+                                $text = $index+1 ;
                                 $columns[] = ['value'=>"t-".$value->survey_answer_id,
                                     'text'=> "$text"
                                 ];
