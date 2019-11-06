860af920e9ec8dd731041a655f1092db588bb670
MohamedAaa <m.3laa.95@gmail.com>
2019-11-06 15:23:53 +0200

    answer list type file

--- api/controllers/AssessmentsController.php
+++ api/controllers/AssessmentsController.php
@@ -152,19 +152,21 @@ class AssessmentsController extends  MyActiveController
                //save multiple
               if (count($value) > 0 ) {
                  foreach ($value as $file) {
-                    if(isset($file['type']) && $file['type']  == 'application/pdf' ){
-                        $filename = ImageHelper::Base64IPdfConverter($file['content'],'answers');
+                    if (!isset($file['id'])) {
+                      if(isset($file['type']) && $file['type']  == 'application/pdf' ){
+                          $filename = ImageHelper::Base64IPdfConverter($file['content'],'answers');
 
-                    }else{
-                        $filename = ImageHelper::Base64IMageConverter($file['content'],'answers');
+                      }else{
+                          $filename = ImageHelper::Base64IMageConverter($file['content'],'answers');
+                      }
+                      $userAnswer =  new SurveyUserAnswer();
+                      $userAnswer->survey_user_answer_user_id = \Yii::$app->user->getId();
+                      $userAnswer->survey_user_answer_survey_id = $question->survey_question_survey_id;
+                      $userAnswer->survey_user_answer_question_id = $question->survey_question_id;
+                      $userAnswer->survey_user_answer_value = 'answers/'.$filename;
+                      $userAnswer->survey_user_answer_text = $file['name'];
+                      $userAnswer->save(false);
                     }
-                    $userAnswer =  new SurveyUserAnswer();
-                    $userAnswer->survey_user_answer_user_id = \Yii::$app->user->getId();
-                    $userAnswer->survey_user_answer_survey_id = $question->survey_question_survey_id;
-                    $userAnswer->survey_user_answer_question_id = $question->survey_question_id;
-                    $userAnswer->survey_user_answer_value = 'answers/'.$filename;
-                    $userAnswer->survey_user_answer_text = $file['name'];
-                    $userAnswer->save(false);
                 }
               }
            }//end if
