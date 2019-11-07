aca06d6d0bbeb255930957777bb0ccb9f81440ba
MohamedAaa <m.3laa.95@gmail.com>
2019-11-07 12:26:22 +0200

    api report

--- api/resources/SurveyResource.php
+++ api/resources/SurveyResource.php
@@ -85,7 +85,7 @@ class SurveyResource extends Survey
                         ]
                     ]
                 ];
-                $assessmentQuestions = array_chunk($model->questions, 3);
+                $assessmentQuestions = array_chunk($model->questions, 5);
                 foreach ($assessmentQuestions as $k => $questions) {
                     $data =[];
 
