4f512472119470efaefe4da33f35249df61d9bb6
MohamedAaa <m.3laa.95@gmail.com>
2019-11-12 14:54:42 +0200

    survey_is_visible

--- backend/modules/assessment/models/SurveyStat.php
+++ backend/modules/assessment/models/SurveyStat.php
@@ -236,7 +236,7 @@ class SurveyStat extends \yii\db\ActiveRecord
             ->andWhere(['survey_stat_user_id' => $userId])->one();
         if ($result) {
             $remainingTime = $survey->survey_time_to_pass - ($result->survey_stat_actual_time / 60);
-            return $remainingTime ;
+            return round($remainingTime);
         }
         return $survey->survey_time_to_pass;
     }
