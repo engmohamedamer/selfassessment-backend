bec7ad1c59dc79e8f44e8bed0ff2ad3fb1fcdb10
Mohamed3akl <mohamed3akl@hotmail.com>
2019-11-21 12:45:17 +0200

    fix

--- backend/modules/assessment/views/answers/view/1.php
+++ backend/modules/assessment/views/answers/view/1.php
@@ -12,7 +12,7 @@ use yii\helpers\Html;
 
 /** @var $question \backend\modules\assessment\models\SurveyQuestion */
 /** @var $form \yii\widgets\ActiveForm */
-
+\backend\assets\AssessmentAsset::register($this);
 $totalVotesCount = $question->getTotalUserAnswersCount();
 
 echo Html::beginTag('div', ['class' => 'answers-stat']);
@@ -40,5 +40,72 @@ foreach ($question->answers as $i => $answer) {
 }
 
 ?>
+<div class="row">
+    <div class="col-md-8">
+
+        <div class="chart-responsive">
+            <canvas id="pieChart" height="150"></canvas>
+        </div>
+
+    </div>
+    <div class="col-md-4">
+        <ul class="chart-legend clearfix">
+            <li><i class="far fa-circle" style="color:#f56954"></i> Chrome</li>
+            <li><i class="far fa-circle" style="color:#00a65a"></i> IE</li>
+            <li><i class="far fa-circle" style="color:#f39c12"></i> FireFox</li>
+            <li><i class="far fa-circle" style="color:#00c0ef"></i> Safari</li>
+            <li><i class="far fa-circle" style="color:#3c8dbc"></i> Opera</li>
+            <li><i class="far fa-circle" style="color:#d2d6de"></i> Navigator</li>
+        </ul>
+    </div>
 
 </div>
+
+</div>
+
+
+
+<?php
+$this->registerJs(<<<JS
+
+
+ //-------------
+  //- PIE CHART -
+  //-------------
+  // Get context with jQuery - using jQuery's .get() method.
+  var pieChartCanvas = $('#pieChart').get(0).getContext('2d')
+    var pieData        = {
+      labels: [
+          'Chrome', 
+          'IE',
+          'FireFox', 
+          'Safari', 
+          'Opera', 
+          'Navigator', 
+      ],
+      datasets: [
+        {
+          data: [700,500,400,600,300,100],
+          backgroundColor : ['#f56954', '#00a65a', '#f39c12', '#00c0ef', '#3c8dbc', '#d2d6de'],
+        }
+      ]
+    }
+    var pieOptions     = {
+      legend: {
+        display: false
+      }
+    }
+    //Create pie or douhnut chart
+    // You can switch between pie and douhnut using the method below.
+    var pieChart = new Chart(pieChartCanvas, {
+      type: 'doughnut',
+      data: pieData,
+      options: pieOptions      
+    })
+
+
+JS
+);
+
+?>
+
