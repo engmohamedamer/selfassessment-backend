76d8b664accef025e1001c6331cf8a826518d507
MohamedAaa <m.3laa.95@gmail.com>
2019-11-21 12:05:05 +0200

    survey list dashboaed

--- backend/modules/assessment/views/answers/view/3.php
+++ backend/modules/assessment/views/answers/view/3.php
@@ -16,41 +16,39 @@ use yii\helpers\Html;
 $totalVotesCount = $question->getTotalUserAnswersCount();
 
 echo Html::beginTag('div', ['class' => 'answers-stat']);
+
+echo '
+    <p class="text-center">
+        <strong>Answers</strong>
+    </p>
+';
 ?>
 <?php
+$class = ['red','aqua','green','yellow'];
 foreach ($question->answers as $i => $answer) {
+    $countUser = count($question->survey->stats);
     $count = $answer->getTotalUserAnswersCount();
-    try {
-        $percent = ($count / $totalVotesCount) * 100;
-    }catch (\Exception $e){
-        $percent = 0;
-    }
-    echo $answer->survey_answer_name;
-    echo Progress::widget([
-        'id' => 'progress-' . $answer->survey_answer_id,
-        'percent' => $percent,
-        'label' => $count,
-        'barOptions' => ['class' => 'progress-bar-info init']
-    ]);
+    echo '
+        <div class="progress-group">
+            <span class="progress-text">'.$answer->survey_answer_name.'</span>
+            <span class="progress-number"><b>'.$count.'</b>/'.$countUser.'</span>
+            <div class="progress sm">
+                <div class="progress-bar progress-bar-'.$class[rand(0,3)].'" style="width: '. ($count / $countUser) * 100  .'%"></div>
+            </div>
+        </div>
+    ';
 }
 
 ?>
-
-
-
-    <p class="text-center">
-    <strong>Answers</strong>
-    </p>
-
+<!-- 
     <div class="progress-group">
-    <span class="progress-text">Add Products to Cart</span>
-    <span class="progress-number"><b>160</b>/200</span>
+        <span class="progress-text">Add Products to Cart</span>
+        <span class="progress-number"><b>160</b>/200</span>
 
-    <div class="progress sm">
-        <div class="progress-bar progress-bar-aqua" style="width: 80%"></div>
+        <div class="progress sm">
+            <div class="progress-bar progress-bar-aqua" style="width: 80%"></div>
+        </div>
     </div>
-    </div>
-    <!-- /.progress-group -->
     <div class="progress-group">
     <span class="progress-text">Complete Purchase</span>
     <span class="progress-number"><b>310</b>/400</span>
@@ -59,7 +57,6 @@ foreach ($question->answers as $i => $answer) {
         <div class="progress-bar progress-bar-red" style="width: 80%"></div>
     </div>
     </div>
-    <!-- /.progress-group -->
     <div class="progress-group">
     <span class="progress-text">Visit Premium Page</span>
     <span class="progress-number"><b>480</b>/800</span>
@@ -68,7 +65,6 @@ foreach ($question->answers as $i => $answer) {
         <div class="progress-bar progress-bar-green" style="width: 80%"></div>
     </div>
     </div>
-    <!-- /.progress-group -->
     <div class="progress-group">
     <span class="progress-text">Send Inquiries</span>
     <span class="progress-number"><b>250</b>/500</span>
@@ -76,7 +72,6 @@ foreach ($question->answers as $i => $answer) {
     <div class="progress sm">
         <div class="progress-bar progress-bar-yellow" style="width: 80%"></div>
     </div>
-    </div>
-    <!-- /.progress-group -->
+    </div> -->
 
 </div>
