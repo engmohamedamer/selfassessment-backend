52cbe82fcebb0ef7148689243d26f534f1a665d8
Mohamed3akl <mohamed3akl@hotmail.com>
2019-11-21 12:05:49 +0200

    fix

---
+++ backend/assets/AssessmentAsset.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: zein
+ * Date: 7/3/14
+ * Time: 3:14 PM
+ */
+
+namespace backend\assets;
+
+use common\assets\AdminLte;
+use common\assets\Html5shiv;
+use yii\web\AssetBundle;
+use yii\web\YiiAsset;
+
+class AssessmentAsset extends AssetBundle
+{
+    /**
+     * @var string
+     */
+    public $basePath = '@webroot';
+    /**
+     * @var string
+     */
+    public $baseUrl = '@web';
+
+    /**
+     * @var array
+     */
+    public $css = [
+       
+    ];
+    /**
+     * @var array
+     */
+    public $js = [
+        '/js/chart.js/Chart.min.js'
+    ];
+
+    /**
+     * @var array
+     */
+    public $depends = [
+        YiiAsset::class,
+        AdminLte::class,
+        Html5shiv::class,
+        BackendAsset::class
+    ];
+}
