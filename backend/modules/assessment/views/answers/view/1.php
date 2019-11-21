2793232bcc21c17a701dc969071408181bd75916
Mohamed3akl <mohamed3akl@hotmail.com>
2019-11-21 12:56:44 +0200

    fix

--- backend/modules/assessment/views/answers/view/1.php
+++ backend/modules/assessment/views/answers/view/1.php
@@ -25,19 +25,19 @@ echo '
 ?>
 <?php
 $class = ['red','aqua','green','yellow'];
-foreach ($question->answers as $i => $answer) {
-    $countUser = count($question->survey->stats);
-    $count = $answer->getTotalUserAnswersCount();
-    echo '
-        <div class="progress-group">
-            <span class="progress-text">'.$answer->survey_answer_name.'</span>
-            <span class="progress-number"><b>'.$count.'</b>/'.$countUser.'</span>
-            <div class="progress sm">
-                <div class="progress-bar progress-bar-'.$class[rand(0,3)].'" style="width: '. ($count / $countUser) * 100  .'%"></div>
-            </div>
-        </div>
-    ';
-}
+// foreach ($question->answers as $i => $answer) {
+//     $countUser = count($question->survey->stats);
+//     $count = $answer->getTotalUserAnswersCount();
+//     echo '
+//         <div class="progress-group">
+//             <span class="progress-text">'.$answer->survey_answer_name.'</span>
+//             <span class="progress-number"><b>'.$count.'</b>/'.$countUser.'</span>
+//             <div class="progress sm">
+//                 <div class="progress-bar progress-bar-'.$class[rand(0,3)].'" style="width: '. ($count / $countUser) * 100  .'%"></div>
+//             </div>
+//         </div>
+//     ';
+// }
 
 ?>
 <div class="row">
@@ -49,13 +49,12 @@ foreach ($question->answers as $i => $answer) {
 
     </div>
     <div class="col-md-4">
-        <ul class="chart-legend clearfix">
-            <li><i class="far fa-circle" style="color:#f56954"></i> Chrome</li>
-            <li><i class="far fa-circle" style="color:#00a65a"></i> IE</li>
-            <li><i class="far fa-circle" style="color:#f39c12"></i> FireFox</li>
-            <li><i class="far fa-circle" style="color:#00c0ef"></i> Safari</li>
-            <li><i class="far fa-circle" style="color:#3c8dbc"></i> Opera</li>
-            <li><i class="far fa-circle" style="color:#d2d6de"></i> Navigator</li>
+        <ul class="chart-legend clearfix" style="margin-top:70px">
+            <li><i class="far fa-circle" style="color:#f56954"></i> Answer 1</li>
+            <li><i class="far fa-circle" style="color:#00a65a"></i> Answer 2</li>
+            <li><i class="far fa-circle" style="color:#f39c12"></i> Answer 3</li>
+            <li><i class="far fa-circle" style="color:#00c0ef"></i> Answer 4</li>
+           
         </ul>
     </div>
 
@@ -76,17 +75,18 @@ $this->registerJs(<<<JS
   var pieChartCanvas = $('#pieChart').get(0).getContext('2d')
     var pieData        = {
       labels: [
-          'Chrome', 
-          'IE',
-          'FireFox', 
-          'Safari', 
-          'Opera', 
-          'Navigator', 
+          'Answer 1', 
+          'Answer 2',
+          'Answer 3', 
+          'Answer 4', 
+          
       ],
       datasets: [
         {
-          data: [700,500,400,600,300,100],
-          backgroundColor : ['#f56954', '#00a65a', '#f39c12', '#00c0ef', '#3c8dbc', '#d2d6de'],
+          data: [3,5,1,2],
+          backgroundColor : ['#f56954', '#00a65a', '#f39c12', '#00c0ef'],
+          //backgroundColor : ['#f56954', '#00a65a', '#f39c12', '#00c0ef', '#3c8dbc', '#d2d6de'],
+
         }
       ]
     }
