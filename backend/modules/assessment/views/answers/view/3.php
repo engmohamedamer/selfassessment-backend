d68d7c027a7b799ca7817c84dab313d113edaddc
Mohamed3akl <mohamed3akl@hotmail.com>
2019-11-21 11:23:45 +0200

    fix

--- backend/modules/assessment/views/answers/view/3.php
+++ backend/modules/assessment/views/answers/view/3.php
@@ -16,6 +16,8 @@ use yii\helpers\Html;
 $totalVotesCount = $question->getTotalUserAnswersCount();
 
 echo Html::beginTag('div', ['class' => 'answers-stat']);
+?>
+<?php
 foreach ($question->answers as $i => $answer) {
     $count = $answer->getTotalUserAnswersCount();
     try {
@@ -31,4 +33,50 @@ foreach ($question->answers as $i => $answer) {
         'barOptions' => ['class' => 'progress-bar-info init']
     ]);
 }
-echo Html::endTag('div');
+
+?>
+
+
+
+    <p class="text-center">
+    <strong>Answers</strong>
+    </p>
+
+    <div class="progress-group">
+    <span class="progress-text">Add Products to Cart</span>
+    <span class="progress-number"><b>160</b>/200</span>
+
+    <div class="progress sm">
+        <div class="progress-bar progress-bar-aqua" style="width: 80%"></div>
+    </div>
+    </div>
+    <!-- /.progress-group -->
+    <div class="progress-group">
+    <span class="progress-text">Complete Purchase</span>
+    <span class="progress-number"><b>310</b>/400</span>
+
+    <div class="progress sm">
+        <div class="progress-bar progress-bar-red" style="width: 80%"></div>
+    </div>
+    </div>
+    <!-- /.progress-group -->
+    <div class="progress-group">
+    <span class="progress-text">Visit Premium Page</span>
+    <span class="progress-number"><b>480</b>/800</span>
+
+    <div class="progress sm">
+        <div class="progress-bar progress-bar-green" style="width: 80%"></div>
+    </div>
+    </div>
+    <!-- /.progress-group -->
+    <div class="progress-group">
+    <span class="progress-text">Send Inquiries</span>
+    <span class="progress-number"><b>250</b>/500</span>
+
+    <div class="progress sm">
+        <div class="progress-bar progress-bar-yellow" style="width: 80%"></div>
+    </div>
+    </div>
+    <!-- /.progress-group -->
+
+</div>
