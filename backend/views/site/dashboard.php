d24a7af673b9af7de33b5784d500fcd258b75a18
Mohamed3akl <mohamed3akl@hotmail.com>
2019-11-28 16:36:52 +0200

    fix

--- backend/views/site/dashboard.php
+++ backend/views/site/dashboard.php
@@ -182,7 +182,7 @@ $this->title = Yii::t('backend', 'Dashboard');
                 <!-- /.d-flex -->
                 <div class="d-flex justify-content-between align-items-center border-bottom mb-3">
                     <p class="text-warning text-xl">
-                        <i class="fas fa-shopping-cart"></i>
+                        <i class="fas fa-check"></i>
                     </p>
                     <p class="d-flex flex-column text-right">
                         <span class="font-weight-bold">
@@ -214,4 +214,87 @@ $this->title = Yii::t('backend', 'Dashboard');
 </div>
 
 <!-- /.row -->
+
+<?php
+$this->registerJs(<<<JS
+
+$(function () {
+  'use strict'
+
+  var ticksStyle = {
+    fontColor: '#495057',
+    fontStyle: 'bold'
+  }
+
+  var mode      = 'index'
+  var intersect = true
+    var Charts = $('#visitors-chart')
+  var visitorsChart  = new Chart(Charts, {
+    data   : {
+      labels  : ['18th', '20th', '22nd', '24th', '26th', '28th', '30th'],
+      datasets: [{
+        type                : 'line',
+        data                : [100, 120, 170, 167, 180, 177, 160],
+        backgroundColor     : 'transparent',
+        borderColor         : '#007bff',
+        pointBorderColor    : '#007bff',
+        pointBackgroundColor: '#007bff',
+        fill                : false
+        // pointHoverBackgroundColor: '#007bff',
+        // pointHoverBorderColor    : '#007bff'
+      },
+        {
+          type                : 'line',
+          data                : [60, 80, 70, 67, 80, 77, 100],
+          backgroundColor     : 'tansparent',
+          borderColor         : '#ced4da',
+          pointBorderColor    : '#ced4da',
+          pointBackgroundColor: '#ced4da',
+          fill                : false
+          // pointHoverBackgroundColor: '#ced4da',
+          // pointHoverBorderColor    : '#ced4da'
+        }]
+    },
+    options: {
+      maintainAspectRatio: false,
+      tooltips           : {
+        mode     : mode,
+        intersect: intersect
+      },
+      hover              : {
+        mode     : mode,
+        intersect: intersect
+      },
+      legend             : {
+        display: false
+      },
+      scales             : {
+        yAxes: [{
+          // display: false,
+          gridLines: {
+            display      : true,
+            lineWidth    : '4px',
+            color        : 'rgba(0, 0, 0, .2)',
+            zeroLineColor: 'transparent'
+          },
+          ticks    : $.extend({
+            beginAtZero : true,
+            suggestedMax: 200
+          }, ticksStyle)
+        }],
+        xAxes: [{
+          display  : true,
+          gridLines: {
+            display: false
+          },
+          ticks    : ticksStyle
+        }]
+      }
+    }
+  })
+})
+JS
+);
+
+?>
 
