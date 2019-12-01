64d2a8ff9d9f397cce17a0fd6974ae333aa3402a
Mohamed3akl <mohamed3akl@hotmail.com>
2019-12-01 16:24:02 +0200

    add front

--- backend/views/site/dashboard.php
+++ backend/views/site/dashboard.php
@@ -19,7 +19,7 @@ $this->title = Yii::t('backend', 'Dashboard');
             <h1 class="m-0 text-dark"><?= Yii::t('backend','Dashboard') ?></h1>
         </div>
         <div class="col-sm-6">
-            <!-- <a href="NewOrganization.html" class="btn btn-primary" style="float: right">New Organization</a> -->
+            <a href="NewOrganization.html" class="btn btn-primary" style="float: right">New Organization</a>
         </div>
         <!-- /.col -->
     </div>
