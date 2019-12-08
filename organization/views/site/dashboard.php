b5030238636968425e466fda4608b65326bd71e5
MohamedAaa <m.3laa.95@gmail.com>
2019-12-08 11:05:39 +0200

    search in filter org

--- organization/views/site/dashboard.php
+++ organization/views/site/dashboard.php
@@ -91,7 +91,7 @@ $i = 1;
             
             <div class="box box-danger">
             <div class="box-header with-border">
-                <h3 class="box-title"><?= Yii::t('common','Assessments Status') ?></h3>
+                <h3 class="box-title"><?= Yii::t('common','Assessments Contributor Status') .' ( '. $countStats .')' ?></h3>
 
                 <div class="box-tools pull-right">
                 <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i>
