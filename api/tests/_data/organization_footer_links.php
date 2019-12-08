2dbb1728ab48313ca9f461cf42f50cb3ace9dbf5
MohamedAaa <m.3laa.95@gmail.com>
2019-12-08 13:47:37 +0200

    organization_footer_links

--- api/tests/_data/organization_footer_links.php
+++ api/tests/_data/organization_footer_links.php
@@ -7,10 +7,10 @@ return [
         'name_link2' => 'name Link2',
         'link2' => 'Link2',
         'name_link3' => 'name Link3',
-        'ink3' => 'Link3',
+        'link3' => 'Link3',
         'name_link4' => 'name Link4',
         'link4' => 'Link4',
         'name_link5' => 'name Link5',
-        'ink5' => 'Link5',
+        'link5' => 'Link5',
     ],
 ];
