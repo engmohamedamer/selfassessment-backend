73325492273dd99d2b86d4044926be8351a91409
MohamedAaa <m.3laa.95@gmail.com>
2019-12-09 17:58:25 +0200

    remainig time

--- backend/modules/assessment/models/SurveyStat.php
+++ backend/modules/assessment/models/SurveyStat.php
@@ -246,7 +246,8 @@ class SurveyStat extends \yii\db\ActiveRecord
             ->andWhere(['survey_stat_user_id' => $userId])->one();
         if ($result) {
             // $remainingTime = ($survey->survey_time_to_pass * 60) - $result->survey_stat_actual_time;
-            $time = ($result->survey_stat_actual_time /  ($survey->survey_time_to_pass * 60)) * 100;
+            $actual_time = ($result->survey_stat_actual_time /  ($survey->survey_time_to_pass * 60)) * 100;
+            $time = 100 - $actual_time;
             return round($time,2);
             // return $remainingTime;
         }
