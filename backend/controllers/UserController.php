745348b623619e9d4634b523b8af9724efbc211b
MohamedAaa <m.3laa.95@gmail.com>
2020-01-09 02:59:02 +0200

    fix

--- backend/controllers/UserController.php
+++ backend/controllers/UserController.php
@@ -231,6 +231,7 @@ class UserController extends BackendController
                 'body' => \Yii::t('backend', 'Data has been updated Successfully') ,
                 'title' =>'',
             ]);
+            // should handle for org users
             if ($profile->organization_id) {
                 return $this->redirect(['/user/organization-admins?organization_id='.$profile->organization_id]);
             }
