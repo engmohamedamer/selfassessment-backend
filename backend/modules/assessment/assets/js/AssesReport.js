72c7edf1da2ae4ade9e1f939f719f05409c39d1c
Mohamed3akl <mohamed3akl@hotmail.com>
2020-02-11 10:48:48 +0200

    fix contreport

--- backend/modules/assessment/assets/js/AssesReport.js
+++ backend/modules/assessment/assets/js/AssesReport.js
@@ -1,19 +1,19 @@
 var currentHostname = window.location.hostname;
 var api;
 
-  if ( currentHostname  == 'organization.selfassest.localhost') {
-         api ='http://api.selfassest.localhost';
-  }
-  // else if(currentHostname  === 'organization.selfassest.localhost' ){
-  //       api = 'http://api.selfassest.localhost/';
-  // }
-  else{
-        api = 'http://api.sahlit.com';
-  } 
+if (currentHostname == 'organization.selfassest.localhost') {
+    api = 'http://api.selfassest.localhost';
+}
+// else if(currentHostname  === 'organization.selfassest.localhost' ){
+//       api = 'http://api.selfassest.localhost/';
+// }
+else {
+    api = 'http://api.tamkeentechlab.com';
+}
 
-  console.log(currentHostname)
+console.log(currentHostname)
 
-  var app = new Vue({
+var app = new Vue({
     el: "#assessmentReport",
     vuetify: new Vuetify(),
     template: `
@@ -288,118 +288,114 @@ var api;
     `,
 
     data: {
-      Api: api,
-      tocken: $("#assessmentReport").attr("data-tocken"),
-      SurveyId:$("#assessmentReport").attr("data-SurveyId"),
-      UserId:$("#assessmentReport").attr("data-UserId"),
-      Asses:{},
-      search: '',
-      headers: [
-        { text: 'الرقم', align: '', sortable: true, value: 'qNum' },
-        { text: 'السؤال', align: '', value: 'qText' },
-        { text: 'الإجابة', align: '', value: 'qAnswer' },
-        { text: 'الملفات المرفقة', value: 'qAttatchments' },
-        { text: 'النقاط المحصلة', align: 'center', value: 'qGainedPoints' },
-        // { text: 'النقاط الإجمالية', align: 'center', value: 'qTotalPoints' },
-        { text: 'الإجرائات التصحيحية', value: 'qCorrectiveActions' },
-      ],
-      assessmentData: {},
-      dialog:false,
-      surveyResults: {},
-      answerValue: true,
-      questionsReport: [],
-      reportGeneralInfo: {},
-      assessmentTitle: '',
-      assessmentDesc: '',
-      pagination: {
-        rowsPerPage: 100
-      },
-      methods: {
-        customFilter(items, search, filter) {
-            search = search.toString().toLowerCase();
-            return items.filter(row => filter(row["qNum"], search));
-        } 
-      },
-     },
+        Api: api,
+        tocken: $("#assessmentReport").attr("data-tocken"),
+        SurveyId: $("#assessmentReport").attr("data-SurveyId"),
+        UserId: $("#assessmentReport").attr("data-UserId"),
+        Asses: {},
+        search: '',
+        headers: [
+            { text: 'الرقم', align: '', sortable: true, value: 'qNum' },
+            { text: 'السؤال', align: '', value: 'qText' },
+            { text: 'الإجابة', align: '', value: 'qAnswer' },
+            { text: 'الملفات المرفقة', value: 'qAttatchments' },
+            { text: 'النقاط المحصلة', align: 'center', value: 'qGainedPoints' },
+            // { text: 'النقاط الإجمالية', align: 'center', value: 'qTotalPoints' },
+            { text: 'الإجرائات التصحيحية', value: 'qCorrectiveActions' },
+        ],
+        assessmentData: {},
+        dialog: false,
+        surveyResults: {},
+        answerValue: true,
+        questionsReport: [],
+        reportGeneralInfo: {},
+        assessmentTitle: '',
+        assessmentDesc: '',
+        pagination: {
+            rowsPerPage: 100
+        },
+        methods: {
+            customFilter(items, search, filter) {
+                search = search.toString().toLowerCase();
+                return items.filter(row => filter(row["qNum"], search));
+            }
+        },
+    },
     mounted() {
-      $.ajax({
-        url:api+"/assessments/custom-report/"+this.SurveyId+"/"+this.UserId,
-        method: "GET",
-        "headers": {
-          "Content-Type": "application/json",
-          Authorization: "Bearer "+this.tocken
+        $.ajax({
+            url: api + "/assessments/custom-report/" + this.SurveyId + "/" + this.UserId,
+            method: "GET",
+            "headers": {
+                "Content-Type": "application/json",
+                Authorization: "Bearer " + this.tocken
 
-        },
-        beforeSend: function() {
-            $(".survey-preloader").show()
+            },
+            beforeSend: function() {
+                $(".survey-preloader").show()
 
-        },
-        success: res => {
-          if(res.status == 200){
-            
-            this.questionsReport = res.data.answers
-            this.reportGeneralInfo = res.data.generalInfo
-            this.assessmentDesc = res.data.description
-            this.assessmentTitle = res.data.title
-            $("#tabletemplate").show()
-            $("#projectFacts").show()
-            $(".content-header").show()
-            $(".survey-preloader").hide()
-          }
-        },
-        error:res =>{
-          $("#tabletemplate").hide()
-          $("#projectFacts").hide()
-          $("#noreport").show()
-          $(".survey-preloader").hide()
+            },
+            success: res => {
+                if (res.status == 200) {
 
-        }
-      });
-      
+                    this.questionsReport = res.data.answers
+                    this.reportGeneralInfo = res.data.generalInfo
+                    this.assessmentDesc = res.data.description
+                    this.assessmentTitle = res.data.title
+                    $("#tabletemplate").show()
+                    $("#projectFacts").show()
+                    $(".content-header").show()
+                    $(".survey-preloader").hide()
+                }
+            },
+            error: res => {
+                $("#tabletemplate").hide()
+                $("#projectFacts").hide()
+                $("#noreport").show()
+                $(".survey-preloader").hide()
+
+            }
+        });
+
     },
     methods: {
-      savePDF() {
-                  
-          let myReport = document.getElementById('my-report'),
-              options = {
-                  logging: true, 
-                  taintTest: false, 
-                  allowTaint: true,
-              }
+        savePDF() {
 
-              
+            let myReport = document.getElementById('my-report'),
+                options = {
+                    logging: true,
+                    taintTest: false,
+                    allowTaint: true,
+                }
 
-          html2canvas(myReport, options).then(function(canvas) {
-              var hide = document.createElement('style')
-              hide.innerHTML = 'footer { display: none !important;} header {display: none !important; } .report-title {display: none !important;} .search-card {display: none !important;} .v-data-footer {display: none !important;}'
-              document.head.appendChild(hide)
-              print();
-              hide.remove();
-          });
 
-      },
 
-      getLevel(gained, total) {
-          if ((gained/total)*100 < 25 || total == 0) {
-              return 'red'
-          } else if ( (gained/total)*100 >= 25 && (gained/total)*100 < 50 ) {
-              return 'orange'
-          } else if ( (gained/total)*100 >= 50 && (gained/total)*100 < 75 ) {
-              return '#cebe32'
-          }
-          else {
-              return 'green'
-          }
-      },
-     
-  
+            html2canvas(myReport, options).then(function(canvas) {
+                var hide = document.createElement('style')
+                hide.innerHTML = 'footer { display: none !important;} header {display: none !important; } .report-title {display: none !important;} .search-card {display: none !important;} .v-data-footer {display: none !important;}'
+                document.head.appendChild(hide)
+                print();
+                hide.remove();
+            });
+
+        },
+
+        getLevel(gained, total) {
+            if ((gained / total) * 100 < 25 || total == 0) {
+                return 'red'
+            } else if ((gained / total) * 100 >= 25 && (gained / total) * 100 < 50) {
+                return 'orange'
+            } else if ((gained / total) * 100 >= 50 && (gained / total) * 100 < 75) {
+                return '#cebe32'
+            } else {
+                return 'green'
+            }
+        },
+
+
     },
-  
+
     computed: {
-     
-  
+
+
     }
-  });
-
-
-
+});
