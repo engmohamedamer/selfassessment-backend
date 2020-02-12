a3c44300b7e9fbae5f82b0e8105d7f3bc846c6e3
Mohamed Amer <mohamed.amer2050@gmail.com>
2020-02-12 17:08:23 +0200

    fixes

--- organization/controllers/SiteController.php
+++ organization/controllers/SiteController.php
@@ -89,7 +89,7 @@ class SiteController extends OrganizationBackendController
 
     private function organizationSurveys($organization_id)
     {
-        $organizationSurvey = Survey::find()->select('survey_id, survey_is_closed, survey_expired_at, survey_name, count(survey_stat.survey_stat_id) as survey_stat')
+        $organizationSurvey = Survey::find()->select('survey_id, survey_is_closed, survey_expired_at, survey_name, count(survey_stat.survey_stat_id) as survey_stat ,admin_enabled')
             ->join('LEFT JOIN','{{%survey_stat}}','{{%survey_stat}}.survey_stat_survey_id = {{%survey}}.survey_id')
             ->where(['org_id'=>$organization_id , 'admin_enabled'=>1])
             ->andWhere(Filter::dateFilter('survey_created_at'));
