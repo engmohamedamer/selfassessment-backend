25edced76027d5be788a601fc1d54b1d2c919784
MohamedAaa <m.3laa.95@gmail.com>
2020-03-03 09:53:56 +0200

    sso login

--- api/controllers/AuthController.php
+++ api/controllers/AuthController.php
@@ -98,7 +98,8 @@ class AuthController extends  RestController
     }
 
     public function actionChangeToken($code){
-        $checkUser = UserProfile::find()->where(['temporary_token'=>$code])->one();
+        $checkUser = UserProfile::find()->where(['temporary_token'=>$code])
+            ->andWhere(['!=','temporary_token_used',0])->one();
         if (!$checkUser) {
             return ResponseHelper::sendFailedResponse(['message'=>'Not Found'],403);
         }
