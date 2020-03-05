9f6810537707d2fd50277363f3ff831f4999cc19
MohamedAaa <m.3laa.95@gmail.com>
2020-03-05 14:50:24 +0200

    -mfix view assessments

--- backend/modules/assessment/views/answers/view/1.php
+++ backend/modules/assessment/views/answers/view/1.php
@@ -16,13 +16,13 @@ use yii\helpers\Html;
 $totalVotesCount = $question->getTotalUserAnswersCount();
 
 
-function random_color_part() {
-    return str_pad( dechex( mt_rand( 0, 255 ) ), 2, '0', STR_PAD_LEFT);
-}
+// function random_color_part() {
+//     return str_pad( dechex( mt_rand( 0, 255 ) ), 2, '0', STR_PAD_LEFT);
+// }
 
-function random_color() {
-    return random_color_part() . random_color_part() . random_color_part();
-}
+// function random_color() {
+//     return random_color_part() . random_color_part() . random_color_part();
+// }
 
 echo '
 <div class="answers-stat" >
@@ -59,7 +59,7 @@ foreach ($question->answers as $i => $answer) {
     <div class="col-md-4">
         <ul class="chart-legend clearfix" style="margin-top:70px">
           <?php foreach($labels as  $i => $lable):
-            $colors[] = '#'.random_color();
+            $colors[] = '#fff';
           ?>
             <li><i class="far fa-circle" style="color:<?= $colors[$i]?>"></i> <?= $lable ?> <?=  $correct[$i]?> </li>
           <?php endforeach;?>
